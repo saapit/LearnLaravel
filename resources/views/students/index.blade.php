1460e5d6d2b68eb6b7d248bcb843165386be2e98
sapit <syafiqshahrim19@gmail.com>
2021-01-19 11:50:07 +0800

    Insert data

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.main')
+
+@section('title', 'Daftar Mahasiswa')
+
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1 class="mt-3">Daftar Mahasiswa</h1>
+
+            <ul class="list-group">
+                @foreach ($students as $student)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{ $student->nama }}
+                    <a href="/students/{{ $student->id }}" class="badge badge-info">detail</a>
+                    @endforeach
+                </li>
+              </ul>
+        </div>
+    </div>
+</div>
+@endsection
